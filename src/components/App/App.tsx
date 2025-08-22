34298731be2154922b5fe2971e43fc6503496cc9
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-08-22 16:53:46 +0300

    create hw

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,87 +1,66 @@
-import { useState, useEffect } from "react";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import SearchBar from "../SearchBar/SearchBar";
-import getMovies from "../../services/movieService";
-import Loader from "../Loader/Loader";
-import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import toast, { Toaster } from "react-hot-toast";
-import MovieGrid from "../MovieGrid/MovieGrid";
-import MovieModal from "../MovieModal/MovieModal";
-import type { Movie } from "../../types/movie";
-import css from "./App.module.css";
-import ReactPaginate from "react-paginate";
-import type { MovieSearchResponse } from "../../services/movieService";
-
-
-const notify = () => toast.error("No movies found for your request.");
+import SearchBox from '../SearchBox/SearchBox';
+import css from './App.module.css';
+import { useState, useEffect } from 'react';
+import { useDebounce } from 'use-debounce';
+import { fetchNotes } from '../../services/noteService';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import Pagination from '../Pagination/Pagination';
+import NoteList from '../NoteList/NoteList';
+import Modal from '../Modal/Modal';
+import NoteForm from '../NoteForm/NoteForm';
 
 export default function App() {
-  const [query, setQuery] = useState("");
+  const [searchText, setSearchText] = useState('');
+  const [debouncedText] = useDebounce(searchText, 500);
   const [page, setPage] = useState(1);
-  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-
-const {
-  data,
-  isLoading,
-  isError,
-  isSuccess
-} = useQuery<MovieSearchResponse, Error>({
-  queryKey: ["movies", query, page],
-  queryFn: () => getMovies(query, page),
-  enabled: query.length > 0,
-  placeholderData: keepPreviousData,
-});
-
+  const [isModalOpen, setModalOpen] = useState(false);
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
 
   useEffect(() => {
-    if (isSuccess && data.results.length === 0) {
-      notify();
-    }
-  }, [isSuccess, data]);
+    setPage(1);
+  }, [debouncedText]);
 
-  const handleSearch = (userSearch: string) => {
-    setQuery(userSearch);
-    setPage(1); 
-  };
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['notes', debouncedText, page],
+    queryFn: () => fetchNotes(page, debouncedText || ''),
+    placeholderData: keepPreviousData,
+  });
+
+  const totalPages = data?.totalPages || 0;
 
   return (
     <div className={css.app}>
-      <SearchBar onSubmit={handleSearch} />
-
-      {isLoading && <Loader />}
-      {isError && <ErrorMessage />}
-
-      {data && data.results.length > 0 && (
-        <>
-          <MovieGrid
-            movies={data.results}
-            onSelect={(movie) => setSelectedMovie(movie)}
+      <header className={css.toolbar}>
+        <SearchBox searchText={searchText} onUpdate={setSearchText} />
+        {totalPages > 1 && (
+          <Pagination
+            pageCount={totalPages}
+            currentPage={page}
+            onPageChange={setPage}
           />
+        )}
+        <button className={css.button} onClick={openModal}>
+          Create note +
+        </button>
+      </header>
 
-          {data.total_pages > 1 && (
-            <ReactPaginate
-              pageCount={data.total_pages}
-              pageRangeDisplayed={5}
-              marginPagesDisplayed={1}
-              onPageChange={({ selected }) => setPage(selected + 1)}
-              forcePage={page - 1}
-              containerClassName={css.pagination}
-              activeClassName={css.active}
-              nextLabel="→"
-              previousLabel="←"
-            />
-          )}
-        </>
-      )}
+      <main>
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error: {(error as Error).message}</p>}
+        {!isLoading && !isError && data?.notes?.length === 0 && (
+          <p>No notes found</p>
+        )}
+        {data?.notes && data.notes.length > 0 && (
+          <NoteList notes={data.notes} />
+        )}
 
-      {selectedMovie && (
-        <MovieModal
-          movie={selectedMovie}
-          onClose={() => setSelectedMovie(null)}
-        />
-      )}
-
-      <Toaster />
+        {isModalOpen && (
+          <Modal onClose={closeModal}>
+            <NoteForm onClose={closeModal} />
+          </Modal>
+        )}
+      </main>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./components/App/App";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App/App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
